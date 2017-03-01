9837b9d4bf960d05c1a12ea3b7060f62123bb946
shubhomoy <shubhomoy.tubu@gmail.com>
2017-03-01 22:56:40 +0530

    download and convertion done

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var ytdl = require('ytdl-core');
 var ffmpeg = require('fluent-ffmpeg');
 var http = require('http');
 var prompt = require('prompt');
+var request = require('request');
 
-var url = "https://www.youtube.com/watch?v=FM7MFYoylVs";
+var YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
 var mp4 = './video.mp4';
 var mp3 = './music.mp3';
 
@@ -28,19 +29,24 @@ http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=' 
 		console.log('Press the curresponding song to download');
 		prompt.start();
 		prompt.get(['Song number'], function(err, response) {
-			
+			request(YOUTUBE_SEARCH_URL + tracks[response['Song number']-1].name + '&key=' + YOUTUBE_APIKEY, function(error, response, body) {
+				response = JSON.parse(body);
+				if(response.items.length > 0) {
+					var item = response.items[0];
+					console.log('Downloading video...');
+					var url = "https://youtube.com/watch/?v=" + item.id.videoId;
+					ytdl(url, {quality: 'lowest'}).pipe(stream);
+					stream.on('finish', function() {
+						console.log('Download completed');
+						console.log('Converting video...');
+						proc = new ffmpeg({
+							source: mp4
+						}).toFormat('mp3').on('end', function() {
+							console.log('Done!');
+						}).saveToFile(mp3);
+					});
+				}
+			});
 		});
 	});
 });
-
-// console.log('Downloading video...');
-// ytdl(url, {quality: 'lowest'}).pipe(stream);
-// stream.on('finish', function() {
-// 	console.log('Download completed');
-// 	console.log('Converting video...');
-// 	proc = new ffmpeg({
-// 		source: mp4
-// 	}).toFormat('mp3').on('end', function() {
-// 		console.log('Done!');
-// 	}).saveToFile(mp3);
-// });
